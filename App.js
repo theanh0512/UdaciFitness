9f02fc0d918b1abe01c0d53f154e5dd431d391f4
theanh0512@gmail.com <theanh0512@gmail.com>
2018-06-04 18:59:43 +0800

    Dummy Live screen

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import AddEntry from "./components/AddEntry";
 import { purple, white } from "./utils/colors";
 import { Constants } from "expo";
 import EntryDetail from "./components/EntryDetail";
+import Live from "./components/Live";
 
 function UdaciStatusBar({ backgroundColor, ...props }) {
   return (
@@ -39,6 +40,15 @@ const tabsInitialization = {
       tabBarLabel: "Add Entry",
       tabBarIcon: ({ tintColor }) => (
         <FontAwesome name="plus-square" size={30} color={tintColor} />
+      )
+    }
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: "Live",
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="ios-speedometer" size={30} color={tintColor} />
       )
     }
   }
